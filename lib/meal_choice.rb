d4b3d52b9eb9d22080c7c705b87d1f7763564a73
Kizito Njoku <nkizito57@gmail.com>
2019-10-13 02:59:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,3 +4,5 @@ def meal_choice(veg1, veg2, protein ="meat")
   puts "A plate of #{veg1} with #{veg2}."
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
+
+meal_choice("greens", "spinage");
